0614116c1e2e59fc49348bc26f6a17a7e99c499b
kobayashitakaki <o.share.thief@gmail.com>
2019-02-24 09:36:38 +0900

    implement video scraping

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,22 @@
 class Video < ApplicationRecord
+  require 'httparty'
+  require 'uri'
+  require 'nokogiri'
   has_many :viewings
+
+  def self.scrape(id)
+    base_url = "https://www.netflix.com"
+    path = "jp/title/#{id.to_s}"
+    url = URI.join(base_url, path)
+    res = HTTParty.get(url)
+    html = Nokogiri::HTML(res.body)
+    title = html.css("h1.title-title").text
+    # 要素が見つからないときの例外処理を入れること
+
+    runtime = res.body[/"runtime":[0-9]+/]
+    runtime = runtime.split(":")[1].to_i
+    # 文字列が見つからないときの例外処理を入れること
+
+    [["netflix_id", id], ["title", title], ["runtime", runtime]].to_h
+  end
 end

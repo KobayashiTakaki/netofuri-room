7bccaae0c2b0243719eee2861ab5e75675f46080
kobayashitakaki <o.share.thief@gmail.com>
2019-03-02 23:54:13 +0900

    change app name

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require 'csv'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module NetflixRoom
+module NetofuriRoom
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2

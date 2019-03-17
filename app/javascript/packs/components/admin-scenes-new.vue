17058050595aa220cf3f19c29a7f0bb624daebd5
kobayashitakaki <o.share.thief@gmail.com>
2019-03-17 23:14:24 +0900

    change admin scenes new using vue

---
+++ app/javascript/packs/components/admin-scenes-new.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="col-lg-8">
+    <h4>{{ video.title }}</h4>
+    <div class="video-episode" v-if="video.type == 'show'">
+      シーズン{{ video.season }}, 第{{ video.episode }}話
+    </div>
+    <div class="mt-2">
+      <form accept-charset="UTF-8" method="post" :action="postPath">
+        <input type="hidden" name="authenticity_token" :value="authenticationToken">
+        <div class="form-group">
+          <label for="scene_title">タイトル</label>
+          <input type="text" class="form-control" name="scene[title]" id="scene_title"
+            v-model="title"
+          >
+        </div>
+
+        <div class="form-group">
+          <label for="scene_start_time">開始時間</label>
+          <input type="text" class="form-control" name="scene[start_time]" id="scene_start_time"
+            v-model="startTime"
+          >
+          {{ startTimeDisplay }}
+        </div>
+
+        <div class="form-group">
+          <label for="scene_end_time">終了時間</label>
+          <input type="text" class="form-control" name="scene[end_time]" id="scene_end_time"
+            v-model="endTime"
+          >
+          {{ endTimeDisplay }}
+        </div>
+
+        <button class="btn btn-primary" type="submit">送信</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: '',
+        startTime: 0,
+        endTime: 0,
+        formDisabled: false
+      }
+    },
+    props: ['authenticationToken', 'postPath', 'video'],
+    methods: {
+      displayTime(comment) {
+        return this.secToTime(comment.time)
+      },
+      secToTime(timeInSeconds) {
+        if(timeInSeconds == null) {
+          return "--:--:--"
+        }
+        const pad = function(num, size) { return ('000' + num).slice(size * -1); }
+        const time = parseFloat(timeInSeconds).toFixed(3)
+        const hours = Math.floor(time / 60 / 60)
+        const minutes = Math.floor(time / 60) % 60
+        const seconds = Math.floor(time - minutes * 60)
+        return pad(hours, 2) + ':' + pad(minutes, 2) + ':' + pad(seconds, 2)
+      },
+      extractNewComment(comments) {
+        const existIds = this.comments.map(
+          comment => comment.id
+        )
+        return comments.filter(
+          comment => !existIds.includes(comment.id)
+        )
+      }
+    },
+    computed: {
+      startTimeDisplay() {
+        return this.secToTime(this.startTime)
+      },
+      endTimeDisplay() {
+        return this.secToTime(this.endTime)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .comments {
+    background-color: #FFFFFF;
+    border-radius: 7px;
+    border: 1px solid rgba(0,0,0,0.04);
+    box-sizing: border-box;
+    max-height: 500px;
+    word-wrap: break-word;
+    word-break: break-all;
+    overflow: auto;
+  }
+
+  .comment:not(:last-child) {
+    border-bottom: 1px solid rgba(0,0,0,0.04);
+  }
+
+  .comment-at {
+    font-size: 80%;
+    color: #888888;
+  }
+
+  .username {
+    font-size: 80%;
+    color: #888888;
+  }
+</style>
---
+++ app/javascript/packs/options/AdminScenesNew.js
@@ -0,0 +1,7 @@
+import AdminScenesNew from '../components/admin-scenes-new.vue'
+
+export default {
+  components: {
+    AdminScenesNew
+  }
+}
